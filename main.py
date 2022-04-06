1b2482367682cf242a3ade17202d7a6650b2e672
rumeza <97740994+rumezaa@users.noreply.github.com>
2022-04-06 13:00:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 #mainimports
 
 import json
-import os
-import smtplib
-from email.message import EmailMessage
 import kivy
 import kivymd
 import plyer
@@ -497,8 +494,7 @@ class FeedBack(Screen):
         try:
             #email response
             msg_stuff = self.ids.feedback.text
-
-            url = "https://blu3whale-default-rtdb.firebaseio.com/.json"
+            url = "https://blu3whalefeedback-5d325-default-rtdb.firebaseio.com/.json"
 
             #sending the feedback
             requests.post(url = url, json = {'user':msg_stuff})
